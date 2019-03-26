bdd8ff7cebac4f4525540abe5638d7ac0087337a
mariukhin <maryuhin.z@gmail.com>
2019-03-26 11:43:45 +0200

    homework-5 update

--- homework-5/app/routes/router.js
+++ homework-5/app/routes/router.js
@@ -5,6 +5,7 @@ const getAllProducts = require('./products/getAllProducts');
 const getProduct = require('./products/getProduct');
 const getUser = require('./user/getUser');
 const getOrder = require('./order/getOrder');
+const getComment = require('./comment/getComment');
 const createOrder = require('./order/createOrder');
 const createProduct = require('./products/createProduct');
 const updateUser = require('./user/updateUser');
@@ -13,7 +14,8 @@ const auth = require('./user/auth');
 const getCurrent = require('./user/getCurrent');
 const verifyToken = require('../modules/check-token');
 const createIngredient = require('./ingredients/createIngredient');
-
+const createComment = require('./comment/createComment');
+const logout = require('./user/logout');
 
 const apiRoutes = express.Router();
 
@@ -25,12 +27,14 @@ apiRoutes
   .get('/products/:id', getProduct)
   .get('/users/:id', getUser)
   .get('/orders/:id', getOrder)
-  // .get('/auth/logout', logout)
+  .get('/auth/logout', logout)
   .get('/auth/current', getCurrent)
+  .get('/comments/:id', getComment)
   .post('/auth/register', createUser)
   .post('/orders', createOrder)
   .post('/products', createProduct)
   .post('/ingredients', createIngredient)
+  .post('/comments', createComment)
   .put('/users/:id', updateUser)
   .put('/products/:id', updateProduct);
 
